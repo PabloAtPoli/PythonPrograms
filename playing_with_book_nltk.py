44bb56044995fbc08928a59715c0e3f74c841331
Pablo Ortiz <paortiz@elpoli.edu.co>
2023-09-01 05:51:11 -0500

    class and instance variables

--- playing_with_book_nltk.py
+++ playing_with_book_nltk.py
@@ -25,56 +25,56 @@ import matplotlib.pyplot as plt
 # plt.show()
 
 
-print("Number of words in text 2 ", len(text3))
+# print("Number of words in text 2 ", len(text3))
 
-print("Number of unique words in text3: ")
-print(set(text3))
-print("Number of sorted unique words in text3  : ")
-print(sorted(set(text3)))
-print(len(set(text3)))
+# print("Number of unique words in text3: ")
+# print(set(text3))
+# print("Number of sorted unique words in text3  : ")
+# print(sorted(set(text3)))
+# print(len(set(text3)))
 
-print("Generate Text from text3 (The book of Genesis)")
-print(text3.generate())
+# print("Generate Text from text3 (The book of Genesis)")
+# print(text3.generate())
 
 
-print("Lexical richness of text3: ", len(set(text3))/len(text3))
+# print("Lexical richness of text3: ", len(set(text3))/len(text3))
 
-print("Count of word 'smote' in text3")
-print(text3.count("smote"))
-print("Percentage of word 'a' in text4")
-print(100*text4.count('a')/len(text4))
+# print("Count of word 'smote' in text3")
+# print(text3.count("smote"))
+# print("Percentage of word 'a' in text4")
+# print(100*text4.count('a')/len(text4))
 
-def lexical_diversity(text):
-    return len(set(text))/len(text)
+# def lexical_diversity(text):
+#     return len(set(text))/len(text)
 
-def percentage(count, total):
-    return 100*count/total
+# def percentage(count, total):
+#     return 100*count/total
 
-print("Lexical richness of text3: ", lexical_diversity(text3))
-print("Percentage of word 'a' in text4: ", percentage(text4.count('a'), len(text4)))
+# print("Lexical richness of text3: ", lexical_diversity(text3))
+# print("Percentage of word 'a' in text4: ", percentage(text4.count('a'), len(text4)))
 
-sent1 = ['Call', 'me', 'Ishmael', '.']
-print("Length of sent1: ", len(sent1))
-print("Lexical richness of sent1: ", lexical_diversity(sent1))
+# sent1 = ['Call', 'me', 'Ishmael', '.']
+# print("Length of sent1: ", len(sent1))
+# print("Lexical richness of sent1: ", lexical_diversity(sent1))
 
-print("Sentence 1" , sent1)
-print("Sentence 2" , sent2)
-print("Sentence 3" , sent3) 
+# print("Sentence 1" , sent1)
+# print("Sentence 2" , sent2)
+# print("Sentence 3" , sent3) 
 
 # print("Concatenation of sent1 and sent2: ", sent1+sent2)
-print(['Monty', 'Python'] + ['and', 'the', 'Holy', 'Grail'])
-print("Append sent2 to sent1: ", sent1.append(sent2))
-print("Append word 'hello' to sent2", sent2.append('hello'))
+# print(['Monty', 'Python'] + ['and', 'the', 'Holy', 'Grail'])
+# print("Append sent2 to sent1: ", sent1.append(sent2))
+# print("Append word 'hello' to sent2", sent2.append('hello'))
 
-print("Word at index 173 in text4")
-print(text4[173])
-print("Index of word 'awaken' in text4")
-print(text4.index('awaken'))
+# print("Word at index 173 in text4")
+# print(text4[173])
+# print("Index of word 'awaken' in text4")
+# print(text4.index('awaken'))
 
-print("First 10 words in text3")
-print(text3[:10])
-print("Word from index 100 to 200 in text3")
-print(text3[100:200])
+# print("First 10 words in text3")
+# print(text3[:10])
+# print("Word from index 100 to 200 in text3")
+# print(text3[100:200])
 
 
 
--- primitive_data_types.py
+++ primitive_data_types.py
@@ -1,3 +1,7 @@
+
+import math
+
+
 # Define a string variable and assigns the string “Juan Pablo Ortiz Medina”
 name = "Juan Pablo Ortiz Medina"
 
@@ -24,11 +28,28 @@ print(name)
 # Use an expression in the formatted string to calculate the average
 math_mark = 4.5
 science_mark = 3.5
-print(
-    f"Math mark = {math_mark}, science mark = {science_mark}, my average is {(math_mark+science_mark)/2}")
+print(f"Math mark = {math_mark}, science mark = {science_mark}, my average is {(math_mark+science_mark)/2}")
 
 
 myString = " juan PABLO ortiz medina"
 
+print(myString.find("BABLO"))
+
 # In one statement (one line of code) print the following string: "JUAN pablo Ortiz Medina"
 print(myString.rstrip()[:5].upper() + myString.rstrip()[5:12].lower()+ myString.rstrip()[12:].title())
+
+print(math.ceil(2.2))
+print(round(2.2))
+
+# Using the math module, make a program that calculate the factorial of 5
+print(f"Factorial of 5 is: {math.factorial(5)}")
+
+# Write a program that reads an integer and float and prints the average of the two numbers. Use formatted string to print the output.
+# integer = int(input("Enter an integer: "))
+# float_number = float(input("Enter a float number: "))
+# print(f"Average of {integer} and {float_number} is {(integer+float_number)/2}")
+
+
+fruit = "apple"
+print(fruit[1:-1])
+
